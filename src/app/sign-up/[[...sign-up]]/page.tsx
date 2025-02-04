ceeb7422f2c239c04fc578e4a69d8271f5438ee6
Soumya Khatri <soumya91rias@gmail.com>
2025-02-04 22:09:36 +0530

    Errors fixes for Deployment in vercel

--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -5,7 +5,7 @@ export default function SignUpButtonComponent() {
         <div className="min-h-screen flex flex-col items-center justify-start pt-20 bg-gray-50 p-4">
             <div className="max-w-md w-full space-y-8 text-center">
                 <h1 className="text-2xl font-semibold text-gray-900 mb-2">
-                    This page is opening in our own domain (localhost:3000 in case of development) instead of clerk's domain
+                    {`This page is opening in our own domain (localhost:3000 in case of development) instead of clerk's domain`}
                 </h1>
                 <SignUp />
             </div>
